b229eaa43511042fed56a2fc709e5f3857e71ed0
berkayguzel06 <berkayguzel43@gmail.com>
2024-01-23 21:54:43 +0300

    Update nnDemo.py

--- nnDemo.py
+++ nnDemo.py
@@ -1,42 +1,29 @@
 from simplenn.model import Model
-from simplenn.dense import Layer_Dense, Layer_Dropout
-from simplenn.engine import ReLU, Softmax, Accuracy_Categorical, Accuracy_Regression, Categorical_Cross_Entropy, Mean_Square_Error, Adam, SGD
+from simplenn.dense import Layer_Dense
+from simplenn.engine import ReLU, Softmax, Accuracy_Categorical, Categorical_Cross_Entropy, Adam
 from sklearn.datasets import make_blobs, make_circles , make_moons
-from nnfs.datasets import sine_data, spiral_data, vertical_data
 import matplotlib.pyplot as plt
-import nnfs
+from sklearn.model_selection import train_test_split
+import numpy as np
 
-nnfs.init()
-classes = 3
-X_val, y_val = sine_data(samples=100)
-X1_val, y1_val = vertical_data(samples=100,classes=classes)
-X2_val, y2_val = spiral_data(samples=100,classes=classes)
-X3_val, y3_val = make_moons(n_samples=100, noise=0.2)
-X4_val, y4_val = make_circles(n_samples=100, noise=0.1)
-X5_val, y5_val = make_blobs(n_samples=100)
+X1, y1 = make_moons(n_samples=1000, noise=0.25)
+X2, y2 = make_circles(n_samples=1000, noise=0.1)
+X3, y3 = make_blobs(n_samples=1000)
 
-X, y = sine_data(samples=1000)
-X1, y1 = vertical_data(samples=1000,classes=classes)
-X2, y2 = spiral_data(samples=1000,classes=classes)
-X3, y3 = make_moons(n_samples=1000, noise=0.25)
-X4, y4 = make_circles(n_samples=1000, noise=0.1)
-X5, y5 = make_blobs(n_samples=1000)
-plt.subplot(2,3,1)
-plt.scatter(X1[:,0], X1[:,1], c=y1, s=20, cmap='jet')
-plt.subplot(2,3,2)
-plt.scatter(X2[:,0], X2[:,1], c=y2, s=20, cmap='jet')
-plt.subplot(2,3,3)
-plt.scatter(X3[:,0], X3[:,1], c=y3, s=20, cmap='jet')
-plt.subplot(2,3,4)
-plt.scatter(X4[:,0], X4[:,1], c=y4, s=20, cmap='jet')
-plt.subplot(2,3,5)
-plt.scatter(X5[:,0], X5[:,1], c=y5, s=20, cmap='jet')
-plt.subplot(2,3,6)
-plt.scatter(X, y, s=20, cmap='jet')
+noise_factor = 0.25
+noise = np.random.normal(scale=noise_factor, size=X3.shape)
+X3_noisy = X3 + noise
+
+X_train, X_val, y_train, y_val = train_test_split(X3, y3, test_size=0.1)
+
+plt.subplot(2,1,1)
+plt.scatter(X_train[:,0], X_train[:,1], c=y_train, s=20, cmap='jet')
+plt.subplot(2,1,2)
+plt.scatter(X_val[:,0], X_val[:,1], c=y_val, s=20, cmap='jet')
 plt.show()
 
-data, target = X4, y4
-validation_data, validation_target = X4_val, y4_val
+data, target = X_train, y_train
+validation_data, validation_target = X_val, y_val
 input_size = data.shape[1]
 print(f"Input shape: {data.shape}, Target shape: {target.shape}")
 print(f"Data size: {data.size}, Target size: {target.size}")
@@ -45,7 +32,7 @@ print(f"Input size: {input_size}, Output size: {target.shape}")
 model = Model()
 model.add(Layer_Dense(input_size,16))
 model.add(ReLU())
-model.add(Layer_Dense(16,2))
+model.add(Layer_Dense(16,3))
 model.add(Softmax())
 
 model.set(
